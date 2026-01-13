534b782902e8c9b45d6cee24926afed1ad633273
Tushar Verma <tusharmyself06@gmail.com>
2026-01-13 19:53:04 +0530

    added env example and added jest for testing

---
+++ apps/backend/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/index.ts'
+  ],
+  coverageDirectory: 'coverage',
+  verbose: true,
+  testTimeout: 10000
+};
